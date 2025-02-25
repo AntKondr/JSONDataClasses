f87ce025517fbd95b25e66f568ce4b023236f072
Anton Kondratiev <akondratiev@veka.ru>
2025-02-25 09:37:09 +0700

    add: setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
+    name="JSONDataClasses",
+    description="Module for simple json data description with python classes",
+    packages=find_packages()
 )
